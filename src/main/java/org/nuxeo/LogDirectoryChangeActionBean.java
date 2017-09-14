823ad5e30fb6c44377d2de7f3ea793c1241a0d72
Harlan Brown <hbrown@nuxeo.com>
2017-09-14 13:57:36 -0500

    first commit

---
+++ src/main/java/org/nuxeo/LogDirectoryChangeActionBean.java
@@ -0,0 +1,51 @@
+package org.nuxeo;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Observer;
+import org.jboss.seam.annotations.Scope;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.webapp.helpers.EventNames;
+import org.nuxeo.ecm.platform.audit.api.AuditLogger;
+import org.nuxeo.ecm.platform.audit.api.LogEntry;
+import org.nuxeo.runtime.api.Framework;
+
+
+@Name("logdirectorychange")
+@Scope(ScopeType.APPLICATION)
+public class LogDirectoryChangeActionBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final Log log = LogFactory.getLog(LogDirectoryChangeActionBean.class);
+
+    protected AuditLogger logger;
+
+    @Observer(EventNames.DIRECTORY_CHANGED)
+    public void run(String dirName) {
+        String message = "Hello from LogDirectoryChange : ";
+        log.debug(message);
+        log.error(dirName);
+        AuditLogger logger = Framework.getService(AuditLogger.class);	
+        LogEntry entry = newEntry(logger,dirName,new Date());
+        logger.addLogEntries(Collections.singletonList(entry));
+    }
+    protected LogEntry newEntry(AuditLogger logger, String dirName, Date date) {
+        LogEntry entry = logger.newLogEntry();
+        entry.setEventId(EventNames.DIRECTORY_CHANGED);
+        entry.setEventDate(date);
+        entry.setCategory("directory change");
+        entry.setComment(dirName);
+        return entry;
+    }
+
+}
